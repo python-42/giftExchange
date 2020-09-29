144d684de059e38edd4eb3332f10481836d6d1d2
Jake <jake@gmail.com>
2020-09-29 11:51:09 -0500

    Very beginning

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+<?php
+//require statements for data that belongs in head
+require "include/head-data.html";
+?>
+<title>Gift Exchange</title>
+</head>
+<body>
+<div class="container-fluid bg-dark">
+	<div class="jumbotron text-danger border border-primary"><h1 class="jumbotron-heading">GIFT EXCHANGE</h1></div>
+	<div class="row">
+	<div class="col-sm-2 pb-3 bg-secondary text-important">
+	<h2><abbr>Navigation</abbr></h2>
+	<div class="list-group">
+	<a href="#" class="list-group-item list-group-item-action active">Home</a>
+	<a href="account-login.php" class="list-group-item list-group-item-action">Account</a>
+	<a href="about.php" class="list-group-item list-group-item-action">About</a>
+	<a href="help.php" class="list-group-item list-group-item-action">Help</a>
+	</div>
+	</div>
+	<div class="col-sm-10 bg-primary text-body">
+	<h1>Welcome to Gift Exchange!</h1>
+	<p>Gift Exchange is a website which allows you to connect with your friends and family. You can ask for a gift, and give others gifts. Gift exchange takes the guesswork out of giving, making sure that at the end of the day, everyone is happy. Learn more about Gift Exchange at the 'About This Site' page.</p>
+	</div>
+	</div>
+</div>
+<style>
+@media (min-width: 576px){
+.jumbotron-heading{font-size:9rem;}
+}
+</style>
+<script>
+//any script should go here
+</script>
+</body>
+</html>
