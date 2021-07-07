3a6d4cba86938268cb36907d568d3813b7f39902
Python42 <herrmann.jake@protonmail.com>
2021-07-07 09:39:24 -0500

    Fixed require path

--- util/checkusername.php
+++ util/checkusername.php
@@ -1,20 +1,20 @@
 <?php
-require "/var/www/giftExchange/include/sql.php";
+
+require $_SERVER['DOCUMENT_ROOT'].'include/sql.php';
 //this page is used in conjuction with ajax to check that the username is ok for account creation
 //sql
-$selectSQL = "SELECT username FROM login";
+$selectSQL = 'SELECT username FROM login';
 $rawResult = $conn->query($selectSQL);
-if ($rawResult->num_rows == 0){
-	echo "Username OK";
-	}else{
-		$result = $rawResult->fetch_array();
-		$input = $_REQUEST["i"]; //this still doesnt work, however it itsnt really worth my time & effort
-		if ($input != ""){
-			if (array_search($input ,$result)){
-				echo "Username is taken. Please try another.";
-				}else{
-					echo "Username OK";//here the array search came back false, so there are no matching usernames
-					}
-			}
-		}//end of else
-?>
+if ($rawResult->num_rows == 0) {
+    echo 'Username OK';
+} else {
+    $result = $rawResult->fetch_array();
+    $input = $_REQUEST['i']; //this still doesnt work, however it itsnt really worth my time & effort
+    if ($input != '') {
+        if (array_search($input, $result)) {
+            echo 'Username is taken. Please try another.';
+        } else {
+            echo 'Username OK'; //here the array search came back false, so there are no matching usernames
+        }
+    }
+}//end of else
